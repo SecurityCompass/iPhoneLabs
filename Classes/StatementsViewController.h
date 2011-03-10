417cbb460301193595ed021540975538fdb3f480
Stefan Arentz <stefan@arentz.ca>
2011-03-10 17:52:38 -0500

    Statements list now uses the new code to download the most recent statement

--- Classes/StatementsViewController.h
+++ Classes/StatementsViewController.h
@@ -1,16 +1,9 @@
-//
-//  StatementsViewController.h
-//  Bank
-//
-//  Created by Stefan Arentz on 11-03-08.
-//  Copyright 2011 Arentz Consulting. All rights reserved.
-//
+// StatementsViewController.h
 
 #import <UIKit/UIKit.h>
 
-
 @interface StatementsViewController : UITableViewController {
-
+	NSArray* _statements;
 }
 
 @end
--- Classes/StatementsViewController.m
+++ Classes/StatementsViewController.m
@@ -1,76 +1,73 @@
-//
-//  StatementsViewController.m
-//  Bank
-//
-//  Created by Stefan Arentz on 11-03-08.
-//  Copyright 2011 Arentz Consulting. All rights reserved.
-//
+// StatementsViewController.m
 
+#import "Networking.h"
+#import "StatementDetailViewController.h"
 #import "StatementsViewController.h"
-
 
 @implementation StatementsViewController
 
+- (void) saveStatement: (NSString*) statement
+{
+	NSString* documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex: 0];
+	NSString* path = [[documentsDirectory stringByAppendingPathComponent: @"Statement"] stringByAppendingPathExtension: @"statement"];
+	[statement writeToFile: path atomically: YES encoding: NSUTF8StringEncoding error: nil];
+}
+
+- (void) scanStatements
+{
+	NSString* documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex: 0];
+	NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath: documentsDirectory error: nil];
+	
+	[_statements release];
+	_statements = nil;
+	
+	_statements = [[files filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self ENDSWITH '.statement'"]] retain];
+}
+
+- (void) viewDidLoad
+{
+	// Look for .statement files in the Documents directory
+
+	[self scanStatements];
+		
+	// If we have no statements then we download the default one
+	
+	if ([_statements count] == 0)
+	{
+		NSError* error = nil;
+		NSString* applicationError = nil;
+		
+		NSString* statement = BankDownloadStatement(&error, &applicationError);
+		if (statement != nil) {
+			[self saveStatement: statement];
+		}
+
+		// Update the list of known statements
+
+		[self scanStatements];
+	}
+}
+
+- (void) viewDidUnload
+{
+	[_statements release];
+	_statements = nil;
+}
 
 #pragma mark -
-#pragma mark View lifecycle
 
-/*
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-*/
-
-/*
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-*/
-/*
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-*/
-/*
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-}
-*/
-/*
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-}
-*/
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-
-#pragma mark -
-#pragma mark Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
+- (NSInteger) numberOfSectionsInTableView: (UITableView*) tableView
+{
     return 1;
 }
 
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-    return 10;
+- (NSInteger) tableView: (UITableView*) tableView numberOfRowsInSection: (NSInteger) section
+{
+    return [_statements count];
 }
 
-
-// Customize the appearance of table view cells.
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
+- (UITableViewCell*) tableView: (UITableView*) tableView cellForRowAtIndexPath: (NSIndexPath*) indexPath
+{
     static NSString *CellIdentifier = @"Cell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
@@ -78,46 +75,17 @@
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
     }
     
-    // Configure the cell...
+    cell.textLabel.text = [_statements objectAtIndex: indexPath.row];
     
     return cell;
 }
 
 #pragma mark -
-#pragma mark Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-    // ...
-    // Pass the selected object to the new view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
-    [detailViewController release];
-    */
+- (void) tableView: (UITableView*) tableView didSelectRowAtIndexPath: (NSIndexPath*) indexPath
+{
+	StatementDetailViewController* vc= [[[StatementDetailViewController alloc] initWithStatementName: [_statements objectAtIndex: indexPath.row]] autorelease];
+	[self.navigationController pushViewController: vc animated: YES];
 }
-
-
-#pragma mark -
-#pragma mark Memory management
-
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Relinquish ownership any cached data, images, etc. that aren't in use.
-}
-
-- (void)viewDidUnload {
-    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
-    // For example: self.myOutlet = nil;
-}
-
-
-- (void)dealloc {
-    [super dealloc];
-}
-
 
 @end
-
